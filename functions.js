51bc6728ff269c512ce9000882029e78feb1248b
Shubham Singh <shubham2101@gmail.com>
2023-08-24 19:58:04 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function signupUser(name, email, ispaid) {
+    console.log(name);
+}
+signupUser("Shubh", "shubh@gmail.in", false);
+function upperCase(val) {
+    val.toUpperCase();
+}
+upperCase("shubham");
+function addtwo(num) {
+    //   return num + 2;
+    return "hello";
+}
+addtwo(5);
---
+++ functions.ts
@@ -0,0 +1,17 @@
+function signupUser(name: string, email: string, ispaid: boolean) {
+  console.log(name);
+}
+signupUser("Shubh", "shubh@gmail.in", false);
+
+function upperCase(val: string) {
+  val.toUpperCase();
+}
+upperCase("shubham");
+
+function addtwo(num: number) {
+  //   return num + 2;
+  return "hello";
+}
+addtwo(5);
+
+export {};
