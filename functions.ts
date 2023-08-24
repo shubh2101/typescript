5ffca3eb24167278cc37ea854789b44e05a8246e
Shubham Singh <shubham2101@gmail.com>
2023-08-24 22:56:49 +0530

    more on functions

--- functions.ts
+++ functions.ts
@@ -3,15 +3,44 @@ function signupUser(name: string, email: string, ispaid: boolean) {
 }
 signupUser("Shubh", "shubh@gmail.in", false);
 
+// default value
+function signupUser2(name: string, email: string, ispaid: boolean = false) {
+  console.log(name);
+}
+signupUser2("Shubh", "shubh@gmail.in");
+
 function upperCase(val: string) {
   val.toUpperCase();
 }
 upperCase("shubham");
 
-function addtwo(num: number) {
-  //   return num + 2;
-  return "hello";
+function addtwo(num: number): number {
+  return num + 2;
+  //   return "hello";
 }
 addtwo(5);
+
+const getHello = (s: string): string => {
+  return "hello";
+};
+
+const heroes = ["thor", "spiderman", "Captain America"];
+
+heroes.map((hero): string => {
+  return `hero is ${hero}`;
+  // return 2
+});
+
+//never
+
+function handleerror(msg: string): never {
+  throw new Error(msg);
+}
+
+//void
+// The inferred return type is void
+function noop() {
+  return;
+}
 
 export {};
