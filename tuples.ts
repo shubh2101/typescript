9f5d8c582d37fa1c8d1c0db920be9a97cbca5b77
Shubham Singh <shubham2101@gmail.com>
2023-08-26 00:11:13 +0530

    tuples

---
+++ tuples.ts
@@ -0,0 +1,15 @@
+let user: [string, number, boolean];
+
+user = ["shubh", 121, true];
+
+let rgb: [number, number, number] = [255, 123, 185];
+
+// weird part
+
+type User2 = [string, number];
+
+let newUser: User2 = ["shubham", 123];
+newUser[0] = "singh";
+
+// newUser.push(true);
+//error Argument of type 'true' is not assignable to parameter of type 'number | string'.
