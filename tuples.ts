7357162e13e3a6111810d422b98c57121f82c75c
Shubham Singh <shubham2101@gmail.com>
2023-08-26 20:42:21 +0530

    tuples & array methods

--- tuples.ts
+++ tuples.ts
@@ -13,3 +13,5 @@ newUser[0] = "singh";
 
 // newUser.push(true);
 //error Argument of type 'true' is not assignable to parameter of type 'number | string'.
+newUser.push("33");
+newUser.push("jkk");
