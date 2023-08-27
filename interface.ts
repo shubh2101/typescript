27cba915a87b0e752c500ffd31866dc33632afda
Shubham Singh <shubham2101@gmail.com>
2023-08-27 12:50:54 +0530

    interface reopening

--- interface.ts
+++ interface.ts
@@ -8,10 +8,17 @@ interface User {
   getCoupan(coupanName: string, value: number): number;
 }
 
+//reopening of interface
+
+interface User {
+    githubToken : string
+}
+
 const john: User = {
   dbId: 123,
   userId: 456,
   email: "shubh@gmail.com",
+  githubToken : "hsakhdka",
   startTrail: () => {
     return "trial started";
   },
