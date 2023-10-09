8009aed38968e680ae16cc7d8ddeb89d3ed3f83e
Shubham Singh <shubham2101@gmail.com>
2023-10-09 17:38:30 +0530

    type narrowing

---
+++ src/typeNarrowing.ts
@@ -0,0 +1,27 @@
+function detectType(val: string | number) {
+  if (typeof val === "string") {
+    return val.toLowerCase();
+  }
+  return val + 3;
+}
+
+function provideId(id: string | null) {
+  if (!id) {
+    console.log("Please provide id");
+    return;
+  }
+  return id.toLowerCase();
+}
+
+function printAll(strs: string | string[] | null) {
+  if (strs) {
+    if (typeof strs === "object") {
+      for (const s of strs) {
+        console.log(s);
+      }
+    } else if (typeof strs === "string") {
+      console.log(strs);
+    }
+  }
+}
+// downside: Not handling the empty string case correctly.
