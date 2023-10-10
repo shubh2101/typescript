ac6a1b9e40485b6eed0cac2311d0ee6f612c1952
Shubham Singh <shubham2101@gmail.com>
2023-10-10 20:18:37 +0530

    type predicates

--- src/typeNarrowing.ts
+++ src/typeNarrowing.ts
@@ -53,4 +53,25 @@ function logValue(x: Date | string) {
   }
 }
 
+//Type predicates
+type Fish = {
+  swim: () => void;
+};
 
+type Bird = {
+  fly: () => void;
+};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "Fish food";
+  } else {
+    pet;
+    return "Bird food";
+  }
+}
